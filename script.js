5bbcd23e5c903ef07fd7c288b552b24b0e32995b
Brumus14 <57995669+Brumus14@users.noreply.github.com>
2020-06-30 17:13:33 +0100

    Added loading screen for photos section

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
-function imgEnlarge(element) {
-  element.setAttribute("id", "enlarged");
+var imgEnlarged = false;
+let photosRendered = 0;
+let photosNum = document.querySelectorAll(".photo").length;
+let photosLoaded = false;
+var loadingPage = document.querySelector(".loading-page");
 
-  window.addEventListener("click", function (e) {
-    if (element.contains(e.target)) {
+function wait(ms) {
+  var start = new Date().getTime();
+  var end = start;
+  while (end < start + ms) {
+    end = new Date().getTime();
+  }
+}
+
+function imgEnlarge(element) {
+  var clonedImg = element.cloneNode(true);
+  document.querySelector(".photos").appendChild(clonedImg);
+  clonedImg.setAttribute("id", "enlarged");
+
+  window.addEventListener("click", function (click) {
+    if (clonedImg.contains(click.target) || element.contains(click.target)) {
     } else {
-      element.removeAttribute("id");
+      clonedImg.remove();
     }
   });
+}
+
+function rendered() {
+  console.log("image rendered");
+
+  photosRendered += 1;
+
+  if (photosRendered == photosNum) {
+    photosLoaded = true;
+    wait(1000);
+    loadingPage.style.display = "none";
+  }
+}
+
+function startRender() {
+  requestAnimationFrame(rendered);
+}
+
+function loaded() {
+  requestAnimationFrame(startRender);
 }
